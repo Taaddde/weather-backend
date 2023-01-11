b64908335684f519c820a4362e881b55636ca818
taaddde <taaddde@hotmail.com>
2023-01-11 16:11:55 -0300

    [FX] Hostname environment added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { build } = require("./app.js");
 const app = build({ logger: true });
 
-app.listen(process.env.PORT || 3000, "0.0.0.0", (err, address) => {
+app.listen(process.env.PORT || 3000, process.env.HOSTNAME || "0.0.0.0", (err, address) => {
   if (err) {
     console.log(err);
     process.exit(1);
